c40aae3dd81f620fac4c2759faf8a5bbc1463c91
paramsatya <Param.satya@gmail.com>
2018-09-18 09:34:53 +0000

    Update abracadabra.ts

--- abracadabra.ts
+++ abracadabra.ts
@@ -4,5 +4,7 @@ const inventory = require('inventory')
 const items = require('items')
 
 function main() {
-
+  const myInventory = inventory(self)
+  myInventory.add( items.cookie(1) )
+    .add( items.bakedPotato(1) )
 }
